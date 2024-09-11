f377c1b814cc95945e51bafbb94546c6fb7eff20
16476 <w0834091@myscc.ca>
2024-09-11 14:22:23 -0400

    Created starter angular file and completed assignment1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'VarinderPal-Singh-Learning-Angular';
+  aboutme = 'I am Computer programming student and I am in second semester';
+  heading = 'VarinderPalSingh';
 }
