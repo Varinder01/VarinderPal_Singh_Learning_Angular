ba5c3c935793abd130e0bfcfb8caae4b04c9d386
16476 <w0834091@myscc.ca>
2024-09-18 21:59:46 -0400

    changeversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'VarinderPal-Singh-Learning-Angular';
-  aboutme = 'I am Computer programming student and I am in second semester';
+  aboutme = 'I am Computer programming student and I am in Third semester';
   heading = 'VarinderPalSingh';
 }
