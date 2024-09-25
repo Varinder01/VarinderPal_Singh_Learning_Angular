4d8624077f80e7f10c3c671e66c6fa773497ef71
16476 <w0834091@myscc.ca>
2024-09-25 15:41:22 -0400

    step 9 made loop for html ngfor

--- src/app/watch-list/watch-list.component.ts
+++ src/app/watch-list/watch-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Watch} from "../Shared/Modules/watch";
 import {WatchListItemComponent} from "../watch-list-item/watch-list-item.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-watch-list',
   standalone: true,
   imports: [
-    WatchListItemComponent
+    WatchListItemComponent,
+    NgForOf
   ],
   templateUrl: './watch-list.component.html',
   styleUrl: './watch-list.component.css'
