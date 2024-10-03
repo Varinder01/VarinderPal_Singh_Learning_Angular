4aca7c3572540b468a8c6f50616c3c09d0ba84bb
16476 <w0834091@myscc.ca>
2024-10-03 19:51:59 -0400

    did part 4 and 5

---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,9 @@
+import {Watch} from "../Shared/Modules/watch";
+
+export const watchList: Watch[] =
+  [
+    { id: 1, company: "Rolex", region: "Switzerland", color: "Black", type: "Chronograph", available: true },
+    { id: 2, company: "Omega", region: "Switzerland", color: "Blue", type: "Diver", available: false },
+    { id: 3, company: "Tag Heuer", region: "Switzerland", color: "Red", type: "Sport", available: true },
+    { id: 4, company: "Audemars Piguet", region: "Switzerland", color: "Gold", type: "Luxury", available: false }
+  ]
--- src/app/watch-list/watch-list.component.ts
+++ src/app/watch-list/watch-list.component.ts
@@ -15,13 +15,6 @@ import {NgForOf} from "@angular/common";
 })
 export class WatchListComponent {
 
-  watchList: Watch[] = // 'any[]' would have worked as well
-    [
-      { id: 1, company: "Rolex", region: "Switzerland", color: "Black", type: "Chronograph", available: true },
-      { id: 2, company: "Omega", region: "Switzerland", color: "Blue", type: "Diver", available: false },
-      { id: 3, company: "Tag Heuer", region: "Switzerland", color: "Red", type: "Sport", available: true },
-      { id: 4, company: "Audemars Piguet", region: "Switzerland", color: "Gold", type: "Luxury", available: false }
-    ]
 
   selectedWatch?: Watch;
   //function to set which student to display
