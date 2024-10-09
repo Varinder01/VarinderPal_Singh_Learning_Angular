b24d5b6e5f8ce52b1cd3e067bc4f22df3d6ab59c
16476 <w0834091@myscc.ca>
2024-10-09 14:16:43 -0400

    I did some modification in routes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppComponent } from './app/app.component';
 import { provideRouter,Routes} from '@angular/router';
 import {WatchListComponent} from "./app/watch-list/watch-list.component";
 import {WatchListItemComponent} from "./app/watch-list-item/watch-list-item.component";
-import {WatchService} from "./app/service/watch.service";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
 
 
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path:'', redirectTo: '/watch', pathMatch: 'full'},
   { path: 'Watch', component: WatchListComponent },
   { path: 'Watch/:id', component: WatchListItemComponent},
-  {path: '**', component:WatchService}
+  { path: 'modify-list-item', component: ModifyListItemComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 
