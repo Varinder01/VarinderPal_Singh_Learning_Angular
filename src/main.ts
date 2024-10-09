5dde59c94a527aa0468fc87f2780b21ebfee9a25
16476 <w0834091@myscc.ca>
2024-10-09 05:29:15 -0400

    I have Created the list of routes in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter,Routes} from '@angular/router';
+import {WatchListComponent} from "./app/watch-list/watch-list.component";
+import {WatchListItemComponent} from "./app/watch-list-item/watch-list-item.component";
+import {WatchService} from "./app/service/watch.service";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+const routes: Routes = [
+  {path:'', redirectTo: '/watch', pathMatch: 'full'},
+  { path: 'Watch', component: WatchListComponent },
+  { path: 'Watch/:id', component: WatchListItemComponent},
+  {path: '**', component:WatchService}
+];
+
+
+
+
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
