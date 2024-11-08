b360f3f1187eea7e40dc0c4171c6f0f8aeb320f4
16476 <w0834091@myscc.ca>
2024-11-08 18:52:26 -0500

    finished assigment 6

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { WatchService } from '../service/watch.service';
+import { Watch } from '../Shared/Modules/watch';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-modify-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './modify-list-item.component.html',
   styleUrl: './modify-list-item.component.css'
 })
-export class ModifyListItemComponent {
+export class ModifyListItemComponent implements OnInit {
+  watchForm: FormGroup;
+  currentWatchId: number | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private watchService: WatchService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.watchForm = this.fb.group({
+      id: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      company: ['', [Validators.required]],
+      region: ['', [Validators.required]],
+      color: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+
+    });
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.queryParamMap.get('id');
+    if (id) {
+      this.currentWatchId = +id;
+      this.loadWatchForEditing();
+    }
+  }
+
+  loadWatchForEditing(): void {
+    if (this.currentWatchId !== null) {
+      this.watchService.getWatchById(this.currentWatchId).subscribe((watch: Watch | undefined) => {
+        if (watch) {
+          this.watchForm.patchValue(watch);
+        } else {
+          alert('Watch not found!');
+        }
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.watchForm.valid) {
+      const watchData: Watch = this.watchForm.value;
+
+      if (this.currentWatchId === null) {
+        // Create a new watch
+        this.watchService.addWatch(watchData).subscribe(() => {
+          this.router.navigate(['/watch']);
+        });
+      } else {
+        // Update the existing watch
+        this.watchService.updateWatch(watchData).subscribe(() => {
+          this.router.navigate(['/watch']);
+        });
+      }
+    }
+  }
+
+  resetForm(): void {
+    this.watchForm.reset();
+    this.currentWatchId = null;
+  }
 }
