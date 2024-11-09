9e9a3c0de42624fd29e8bdea94a433ea130e2596
16476 <w0834091@myscc.ca>
2024-11-08 23:38:20 -0500

    in data sercive created and added contetn

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Watch } from './Shared/Modules/watch';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
 
-  constructor() { }
+  createDb() {
+    const watches: Watch[] = [
+      { id: 1, company: "Rolex", region: "Switzerland", color: "Black", type: "Chronograph", available: true, imgSrc: "photos/img.png" },
+      { id: 2, company: "Omega", region: "Switzerland", color: "Blue", type: "Diver", available: false, imgSrc: "photos/img_1.png" },
+      { id: 3, company: "Tag Heuer", region: "Switzerland", color: "Red", type: "Sport", available: true, imgSrc: "photos/img_2.png" },
+      { id: 4, company: "Audemars Piguet", region: "Switzerland", color: "Gold", type: "Luxury", available: false, imgSrc: "photos/img_3.png" }
+    ];
+    return { watches };
+  }
 }
